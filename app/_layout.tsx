aad84576d884dab141e3cdcb68821b59733f3ed5
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2024-05-24 12:01:37 -0300

    feat(components/animated-intro): creating an animated intro

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
 import { Stack } from 'expo-router'
+import { StatusBar } from 'react-native'
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <>
+      <StatusBar barStyle="dark-content" />
+
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+    </>
   )
 }
